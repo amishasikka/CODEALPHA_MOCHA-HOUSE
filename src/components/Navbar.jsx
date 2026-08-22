56b26be802653d636626306e93c0200f9cc7a49a
Amisha <141943974+amishasikka@users.noreply.github.com>
2026-08-22 18:22:30 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
+import {
+    CircleUser,
+    ShoppingBag,
+    Coffee,
+    Heart,
+    Leaf,
+    ArrowRight
+} from "lucide-react";
 export default function Navbar(){
 
     const [menuOpen , setMenuOpen] = useState(false);
@@ -9,28 +16,31 @@ export default function Navbar(){
             <section className="navbar">
                 <div className="nav">
                     <Link to="/" className="logo">
-                        <img src="/src/assets/logo.png" alt="Coffee Logo"/>
+                        <img src="/src/assets/logos.png" alt="Coffee Logo"/>
                     </Link>
                     <div className="nav-links">
                         <ul>
-                            <li><Link to="/">home</Link></li>
-                            <li><Link to="/products">menu</Link></li>
-                            <li><Link to="/cart">cart</Link></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/products">Menu</Link></li>
+                            <li><Link to="/cart">Cart</Link></li>
                         </ul>
                     </div>
 
                     <div className={`mobile-menu : ${menuOpen ? "open" : ""}`}>
                         <ul>
-                            <li><Link to="/">home</Link></li>
-                            <li><Link to="/products">menu</Link></li>
-                            <li><Link to="/cart">cart</Link></li>
-                            <li><Link to="/login">login</Link></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/products">Menu</Link></li>
+                            <li><Link to="/cart">Cart</Link></li>
+                            <li><Link to="/login">Login</Link></li>
                             <li><img src="/src/assets/mobile-menu-logo.png" alt="coffee logo" /></li>
                         </ul>
                     </div>
 
                     <div className="nav-btns">
-                        <Link to="/login" className="login-btn">login</Link>
+                        <Link to="/login" className="login-btn">
+                            <CircleUser size={18} strokeWidth={2} color="var(--cards)" />
+                            login
+                        </Link>
                     </div>
 
                     <div className="menu">
