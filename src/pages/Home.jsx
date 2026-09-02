6712767d6548603e7cefd7e6ba4dde3c1da741fe
Amisha <141943974+amishasikka@users.noreply.github.com>
2026-09-02 17:29:02 +0530

    added hero texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home(){
     return (
         <>
             <section className="home">
-                <div className="hero-container">
+                <div className="hero-content">
                     <p className="welcome-text">WELCOME</p>
                     <h1 className="hero-heading">We serve the richest coffee in the city!</h1>
                     <p className="hero-subheading">Experience the perfect blend of flavor and aroma with our premium coffee selection.</p>
