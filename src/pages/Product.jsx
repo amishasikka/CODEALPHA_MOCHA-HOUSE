bcf0d8fdde87ad56ae7b6ba8fe574d9352d84b51
Amisha <141943974+amishasikka@users.noreply.github.com>
2026-09-03 15:51:02 +0530

    started product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,22 @@
 export default function Product(){
     return(
         <>
-            <h1>Product</h1>
+            <section className="product-section">
+                <h2 className="product-heading">Our Products</h2>
+                <div className="product-container">
+                    <div className="products">
+                        <div className="product-card">
+                            <div className="product-image">
+                                <img/>
+                            </div>
+                            <div className="product-info">
+                                <div className="product-name"></div>
+                                <div className="product-price"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
