c86d31f99d5201eaad837a96f5299993e0d96d79
Amisha <141943974+amishasikka@users.noreply.github.com>
2026-09-05 21:19:35 +0530

    changes in sizes of screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Product from './pages/Product'
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/products" element={<Product/>}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
+import Navbar from "../components/Navbar";
 import Product from "./Product";
 
 export default function Home(){
     return (
         <>
-            <section className="home">
-                <div className="hero-content">
-                    <p className="welcome-text">WELCOME</p>
-                    <h1 className="hero-heading">We serve the richest coffee in the city!</h1>
-                    <p className="hero-subheading">Experience the perfect blend of flavor and aroma with our premium coffee selection.</p>
-                    <button className="hero-button">Order Now</button>
-                </div>
-            </section>
+            <div className="hero-wrapper">
+                <Navbar/>
+                <section className="home">
+                    <div className="hero-content">
+                        <p className="welcome-text">WELCOME</p>
+                        <h1 className="hero-heading">We serve the richest coffee in the city!</h1>
+                        <p className="hero-subheading">Experience the perfect blend of flavor and aroma with our premium coffee selection.</p>
+                        <button className="hero-button">Order Now</button>
+                    </div>
+                </section>
+            </div>
             <Product/>
         </>
     )
