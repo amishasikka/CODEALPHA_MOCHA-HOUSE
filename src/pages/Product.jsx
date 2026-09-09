0b28e5b1b4edd1c884aebf8d9685e06823c33b6c
Amisha <141943974+amishasikka@users.noreply.github.com>
2026-09-09 15:53:43 +0530

    minor changes

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -21,6 +21,57 @@ export default function Product(){
                                 </div>
                             </div>
                         </div>
+
+                        <div className="product-card">
+                            <div className="product-image">
+                                <img src="/src/assets/Vector.png" className="heart-icon"></img>
+                                <img src="/src/assets/products/p1.png" className="product-img" alt="Product 1" />
+                            </div>
+                            <div className="product-info">
+                                <div className="product-title">
+                                    <h2 className="product-name">Cappacino</h2>
+                                    <p className="product-description">A delicious coffee drink made with espresso and steamed milk.</p>
+                                </div>
+                                <div className="product-price-order">
+                                    <h3 className="product-price">$3.99</h3>
+                                    <button className="order-button">Order Now</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="product-card">
+                            <div className="product-image">
+                                <img src="/src/assets/Vector.png" className="heart-icon"></img>
+                                <img src="/src/assets/products/p1.png" className="product-img" alt="Product 1" />
+                            </div>
+                            <div className="product-info">
+                                <div className="product-title">
+                                    <h2 className="product-name">Cappacino</h2>
+                                    <p className="product-description">A delicious coffee drink made with espresso and steamed milk.</p>
+                                </div>
+                                <div className="product-price-order">
+                                    <h3 className="product-price">$3.99</h3>
+                                    <button className="order-button">Order Now</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="product-card">
+                            <div className="product-image">
+                                <img src="/src/assets/Vector.png" className="heart-icon"></img>
+                                <img src="/src/assets/products/p1.png" className="product-img" alt="Product 1" />
+                            </div>
+                            <div className="product-info">
+                                <div className="product-title">
+                                    <h2 className="product-name">Cappacino</h2>
+                                    <p className="product-description">A delicious coffee drink made with espresso and steamed milk.</p>
+                                </div>
+                                <div className="product-price-order">
+                                    <h3 className="product-price">$3.99</h3>
+                                    <button className="order-button">Order Now</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
